c3eda628d0cf2a7089203255bef8f253303de7b4
ANDREW JAMES BOYLE <ANDREWBOYLEMUSIC@GMAIL.COM>
2022-08-24 17:42:33 -0700

    test 2

--- app.js
+++ app.js
@@ -25,8 +25,5 @@ signOutLink.addEventListener('click', signOutUser);
 // andrew's comment
 
 // comment test
-<<<<<<< HEAD
 
 // tanner test
-=======
->>>>>>> f1869a6123ee746be1814cc1d8835762730256a8
